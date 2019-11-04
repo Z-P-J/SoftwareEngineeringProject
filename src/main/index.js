3d06220449e66e8d8a999758cd04384db831e530
Z-P-J <37832345+Z-P-J@users.noreply.github.com>
2019-11-04 15:42:08 +0800

    Update with the electron-vue boilerplate.

---
+++ src/main/index.js
@@ -0,0 +1,165 @@
+'use strict'
+
+import { app, BrowserWindow, Menu, dialog, ipcMain, Tray } from 'electron'
+import * as fs from 'fs'
+import path from 'path'
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const STORE_PATH = app.getPath('userData') // 获取electron应用的用户目录
+console.log('userData path: ', STORE_PATH)
+
+const fsExtra = require('fs-extra')
+if (!fsExtra.pathExistsSync(STORE_PATH)) { // 如果不存在路径
+  fsExtra.mkdirpSync(STORE_PATH) // 就创建
+}
+const adapter = new FileSync(path.join(STORE_PATH, '/music-player-db.json')) // 初始化lowdb读写的json文件名以及存储路径
+const db = low(adapter)
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+console.log('NODE_ENV=' + process.env.NODE_ENV)
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9081`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  const options = {
+    height: 800,
+    width: 1200,
+    show: true,
+    center: true,
+    fullscreenable: false,
+    resizable: true,
+    title: 'music-player',
+    vibrancy: 'ultra-dark',
+    backgroundColor: '#ffffff', // 背景色
+    webPreferences: {
+      backgroundThrottling: false,
+      webSecurity: false
+    }
+  }
+  if (process.platform === 'darwin') { // 针对mac平台做出不同的配置
+  }
+  if (process.platform === 'linux') { // 针对linux平台做出不同的配置
+    options.transparent = true
+    options.titleBarStyle = 'hidden'
+    Menu.setApplicationMenu(null)
+  }
+  if (process.platform === 'win32') { // 针对windows平台做出不同的配置
+    options.frame = true // 创建一个frameless窗口
+    options.transparent = true
+    options.titleBarStyle = 'hidden'
+    Menu.setApplicationMenu(null)
+  }
+
+  mainWindow = new BrowserWindow(options)
+  // const menu = Menu.buildFromTemplate(template)
+  // Menu.setApplicationMenu(menu)
+
+  if (process.platform === 'win32') {
+    let tray = null
+    // 创建系统通知区菜单
+    tray = new Tray(`${__static}/icon.ico`)
+
+    mainWindow.on('show', () => {
+      tray.setHighlightMode('always')
+    })
+
+    mainWindow.on('hide', () => {
+      tray.setHighlightMode('never')
+    })
+
+    const contextMenu = Menu.buildFromTemplate([
+      {label: '退出', click: () => { mainWindow.destroy() }} // 我们需要在这里有一个真正的退出（这里直接强制退出）
+    ])
+
+    tray.setToolTip('music-player')
+    tray.setContextMenu(contextMenu)
+    tray.on('click', () => { // 我们这里模拟桌面程序点击通知区图标实现打开关闭应用的功能
+      mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+      mainWindow.isVisible() ? mainWindow.setSkipTaskbar(false) : mainWindow.setSkipTaskbar(true)
+    })
+  }
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('close', (event) => {
+    mainWindow.webContents.send('main-process-messages', 'close')
+    if (process.platform === 'win32') {
+      mainWindow.hide()
+      mainWindow.setSkipTaskbar(true)
+      event.preventDefault()
+    }
+  })
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+app.request_remote = require('request')
+app.got_remote = require('got')
+app.fs = fs
+app.dialog = dialog
+
+db.defaults({setting: { saveDir: '', redotAbout: true, remoteSearch: true }, playlist: []})
+  .write()
+
+ipcMain.on('DataOP', (event, arg) => {
+  console.log(arg)
+  if (arg['method'] === 'set') {
+    db.set('setting', arg['setting']).write()
+    db.set('playlist', arg['audios']).write()
+    event.returnValue = 'success'
+  }
+  if (arg['method'] === 'get') {
+    let setting = db.get('setting').value()
+    let playlist = db.get('playlist').value()
+    event.returnValue = {setting: setting, playlist: playlist}
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
---
+++ src/renderer/components/MainPage.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <el-dropdown @command="setting" style="float: right">
+                    <span class="el-dropdown-link">
+                        <i style="font-size: 22px" class="el-icon-setting"></i>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item command="about">
+                            关于
+                        </el-dropdown-item>
+                        <el-dropdown-item command="setting">
+                            设置
+                        </el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+
+    <div style="font-size: 22px">音乐播放器</div>
+
+        <el-dialog @closed="dataReload"
+                   title="设置"
+                   :visible.sync="dialogAbout"
+                   width="40%">
+            <div>关于弹窗</div>
+        </el-dialog>
+        <el-dialog @closed="dataReload"
+                   title="设置"
+                   :visible.sync="dialogSetting"
+                   width="40%">
+            <div>设置弹窗</div>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'MainPage',
+      data () {
+        return {
+          dialogSetting: false,
+          dialogAbout: false
+        }
+      },
+      methods: {
+        setting (name) {
+          if (name === 'about') {
+            this.dialogAbout = true
+          } else if (name === 'setting') {
+            this.dialogSetting = true
+          }
+        }
+      }
+    }
+</script>
+
+<style>
+
+    ::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+    }
+
+    ::-webkit-scrollbar-track {
+        border-radius: 4px;
+        box-shadow: inset 0 0 6px rgba(0, 0, 0, .2);
+        background: #ffff;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        border-radius: 4px;
+        box-shadow: inset 0 0 6px rgba(0, 0, 0, .2);
+        background-color: #d9d9d9;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background-color: #919191;
+    }
+
+    *, *::after, *::before {
+        box-sizing: border-box;
+        max-height: 999999px;
+    }
+
+    html {
+        font-size: 62.5%;
+        -webkit-text-size-adjust: 100%;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-rendering: optimizeLegibility;
+        scroll-behavior: smooth;
+        line-height: 1.15;
+    }
+</style>
