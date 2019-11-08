99ded4257eadf831d36a67ba02a95957031d6dbc
Z-P-J <37832345+Z-P-J@users.noreply.github.com>
2019-11-08 23:57:32 +0800

    Add a sidebar.

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,92 +1,112 @@
 <template>
-    <div>
+  <div>
+    <el-container class="container" id="main">
+      <el-aside width="230px" class="aside">
+        <side-bar />
+      </el-aside>
+      <el-container>
         <el-dropdown @command="setting" style="float: right">
-                    <span class="el-dropdown-link">
-                        <i style="font-size: 22px" class="el-icon-setting"></i>
-                    </span>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item command="about">
-                            关于
-                        </el-dropdown-item>
-                        <el-dropdown-item command="setting">
-                            设置
-                        </el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-
-    <div style="font-size: 22px">音乐播放器</div>
-
-        <el-dialog @closed="dataReload"
-                   title="设置"
-                   :visible.sync="dialogAbout"
-                   width="40%">
-            <div>关于弹窗</div>
+          <span class="el-dropdown-link">
+            <i style="font-size: 22px" class="el-icon-setting"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="about">关于</el-dropdown-item>
+            <el-dropdown-item command="setting">设置</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <el-dialog @closed="dataReload" title="设置" :visible.sync="dialogAbout" width="40%">
+          <div>关于弹窗</div>
         </el-dialog>
-        <el-dialog @closed="dataReload"
-                   title="设置"
-                   :visible.sync="dialogSetting"
-                   width="40%">
-            <div>设置弹窗</div>
+        <el-dialog @closed="dataReload" title="设置" :visible.sync="dialogSetting" width="40%">
+          <div>设置弹窗</div>
         </el-dialog>
-    </div>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
-    export default {
-      name: 'MainPage',
-      data () {
-        return {
-          dialogSetting: false,
-          dialogAbout: false
-        }
-      },
-      methods: {
-        setting (name) {
-          if (name === 'about') {
-            this.dialogAbout = true
-          } else if (name === 'setting') {
-            this.dialogSetting = true
-          }
-        }
+import SideBar from "../components/sidebar/";
+export default {
+  name: "MainPage",
+  components: {
+    SideBar
+  },
+  data() {
+    return {
+      dialogSetting: false,
+      dialogAbout: false
+    };
+  },
+  methods: {
+    setting(name) {
+      if (name === "about") {
+        this.dialogAbout = true;
+      } else if (name === "setting") {
+        this.dialogSetting = true;
       }
     }
+  }
+};
 </script>
 
 <style>
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
 
-    ::-webkit-scrollbar {
-        width: 8px;
-        height: 8px;
-    }
+::-webkit-scrollbar-track {
+  border-radius: 4px;
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+  background: #ffff;
+}
 
-    ::-webkit-scrollbar-track {
-        border-radius: 4px;
-        box-shadow: inset 0 0 6px rgba(0, 0, 0, .2);
-        background: #ffff;
-    }
+::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+  background-color: #d9d9d9;
+}
 
-    ::-webkit-scrollbar-thumb {
-        border-radius: 4px;
-        box-shadow: inset 0 0 6px rgba(0, 0, 0, .2);
-        background-color: #d9d9d9;
-    }
+::-webkit-scrollbar-thumb:hover {
+  background-color: #919191;
+}
 
-    ::-webkit-scrollbar-thumb:hover {
-        background-color: #919191;
-    }
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+  max-height: 999999px;
+}
 
-    *, *::after, *::before {
-        box-sizing: border-box;
-        max-height: 999999px;
-    }
+body {
+  margin: 0;
+  font-size: 62.5%;
+  -webkit-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
+  scroll-behavior: smooth;
+  line-height: 1.15;
+}
 
-    html {
-        font-size: 62.5%;
-        -webkit-text-size-adjust: 100%;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-rendering: optimizeLegibility;
-        scroll-behavior: smooth;
-        line-height: 1.15;
-    }
+.container {
+  height: 100vh;
+}
+
+.aside {
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  /* background: linear-gradient(to right, #ff9a9e, #fad0c4); */
+}
+
+.main {
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  display: flex;
+  flex: 1;
+  flex-shrink: 0;
+}
 </style>
---
+++ src/renderer/components/sidebar/index.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="side-bar">
+    <el-scrollbar class="menu-view scroll-page">
+      <div class="user-info">
+        <div>
+          <el-avatar icon="el-icon-user-solid"></el-avatar>
+        </div>
+        <div class="avatar no-drag" @click="login">
+          <img src="../../assets/max.png" />
+        </div>
+        <div class="mt-5">
+          <el-button
+            style="color: #67C23A;"
+            type="text"
+            class="no-drag"
+            size="mini"
+            @click="login"
+          >立即登录</el-button>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">发现音乐</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">本地音乐</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-download"></i>
+          <span slot="title">下载音乐</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-star-off"></i>
+          <span slot="title">我的收藏</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-s-data"></i>
+          <span slot="title">最近播放</span>
+        </el-menu-item>
+        <el-submenu index="6" id="created_song_list">
+          <template slot="title">
+            <i class="el-icon-tickets"></i>
+            <span>创建的歌单</span>
+            <!-- <i class="el-icon-plus"></i> -->
+          </template>
+          <el-menu-item index="6-1" @click="createSongList">
+            <i class="el-icon-folder-add"></i>
+            新建歌单
+          </el-menu-item>
+          <el-menu-item index="6-2" style="display: none">
+            <el-input placeholder="请输入歌单名" v-model="input" autocomplete="off">
+              <i class="el-icon-folder-add"></i>
+            </el-input>
+          </el-menu-item>
+          <el-menu-item v-for="(item, pos) in createdSongList" v-bind:key="item.index" :index="item.index">歌单{{pos}}</el-menu-item>
+          <!-- <el-menu-item index="6-2">歌单1</el-menu-item>
+          <el-menu-item index="6-3">歌单2</el-menu-item>
+          <el-menu-item index="6-4">歌单3</el-menu-item>
+          <el-menu-item index="6-5">歌单4</el-menu-item>
+          <el-menu-item index="6-6">歌单5</el-menu-item>
+          <el-menu-item index="6-7">歌单6</el-menu-item>
+          <el-menu-item index="6-8">歌单7</el-menu-item> -->
+        </el-submenu>
+        <el-submenu index="7" id="starred_song_list">
+          <template slot="title">
+            <i class="el-icon-s-order"></i>
+            <span>收藏的歌单</span>
+          </template>
+          <el-menu-item index="7-1">歌单0</el-menu-item>
+          <el-menu-item index="7-2">歌单1</el-menu-item>
+          <el-menu-item index="7-3">歌单2</el-menu-item>
+          <el-menu-item index="7-4">歌单3</el-menu-item>
+          <el-menu-item index="7-5">歌单4</el-menu-item>
+          <el-menu-item index="7-6">歌单5</el-menu-item>
+          <el-menu-item index="7-7">歌单6</el-menu-item>
+          <el-menu-item index="7-8">歌单7</el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "输入",
+      createdSongList: [
+        { index: "6-3" },
+        { index: "6-4" },
+        { index: "6-5" },
+        { index: "6-6" },
+        { index: "6-7" },
+        { index: "6-8" },
+        { index: "6-9" }
+      ]
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    createSongList() {
+      this.$message({
+        message: "新建歌单成功！",
+        type: "success"
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.scroll-page {
+  overflow: hidden;
+  margin: 0;
+}
+
+.side-bar {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.aside-view {
+  flex: 1;
+  display: flex;
+}
+
+.menu-view {
+  flex: 1;
+  overflow: hidden;
+}
+
+.menu-group {
+  margin-bottom: 25px;
+  padding: 0 20px;
+}
+
+.menu-title {
+  margin-bottom: 10px;
+  font-size: 12px;
+  padding-left: 10px;
+  display: flex;
+  align-items: center;
+  color: #666;
+}
+
+.menu-title .left {
+  flex: 1;
+}
+
+.menu-title .right {
+  width: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.menu-title .right .iconfont {
+  font-size: 14px;
+  color: #888;
+  margin-left: 10px;
+}
+
+.menu-item {
+  padding: 5px 10px;
+  font-size: 13px;
+  border-radius: 2px;
+  margin-bottom: 5px;
+  cursor: pointer;
+}
+
+.menu-item .left .iconfont {
+  margin-right: 10px;
+}
+
+.menu-item .left .iconfont .hot {
+  color: #ff4400;
+}
+
+.menu-item:hover {
+  background: #e6e7e7;
+}
+
+.menu-item.active {
+  background: #4285f4;
+  color: #fff;
+}
+
+.user-info {
+  -webkit-app-region: drag;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.user-info .avatar {
+  width: 50px;
+  height: 50px;
+}
+
+.user-info .avatar img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  cursor: pointer;
+}
+</style>
