f4b4fef1c860f9bfbd0675d24374adafbd2e3685
xioyodd <767272299@qq.com>
2019-11-16 16:21:32 +0800

    commit

--- src/renderer/components/main/home/singer/components/singer-item.vue
+++ src/renderer/components/main/home/singer/components/singer-item.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="singer-item">
-    <img :src="item.img1v1Url" />
-    <div class="name">{{item.name}}</div>
+    <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="singer-img"/>
+    <div class="name">{{item}}</div>
   </div>
 </template>
 <script>
@@ -16,4 +16,20 @@ export default {
 };
 </script>
 <style>
+.singer-item {
+  /* width: 170px;
+  height: 160px; */
+  margin-bottom: 18px;
+  background: #f0f0f0;
+  box-sizing: border-box;
+  padding: 5px;
+  font-size: 14px;
+  text-align: center;
+  border-radius: 2px;
+}
+.singer-img {
+  width: 110px;
+  height: 110px;
+  border-radius: 50%;
+}
 </style>
--- src/renderer/components/main/home/singer/components/singer-list.vue
+++ src/renderer/components/main/home/singer/components/singer-list.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="singer-page">
     <div class="class-list">
       <!-- <div
         class="class-item hover-bg"
@@ -8,9 +8,10 @@
         :key="index"
         @click="catClick(item.id,index)"
       >{{item.name}}</div>-->
-      <div>
-        语种：
+      <div class="language">
+        <p>语种：</p>
         <el-radio-group size="mini" v-model="language">
+          <el-radio-button label="全部"></el-radio-button>
           <el-radio-button label="中"></el-radio-button>
           <el-radio-button label="日"></el-radio-button>
           <el-radio-button label="韩"></el-radio-button>
@@ -18,28 +19,55 @@
           <el-radio-button label="其他"></el-radio-button>
         </el-radio-group>
       </div>
-      <div>
-        分类：
+      <div class="gender">
+        <p>分类：</p>
         <el-radio-group size="mini" v-model="gender">
+          <el-radio-button label="全部"></el-radio-button>
           <el-radio-button label="男"></el-radio-button>
           <el-radio-button label="女"></el-radio-button>
           <el-radio-button label="组合/乐队"></el-radio-button>
         </el-radio-group>
       </div>
-      <el-divider></el-divider>
-      <!-- <div>首字母
-        <el-radio-group v-model="radio1">
-          <el-radio-button label="上海"></el-radio-button>
-          <el-radio-button label="北京"></el-radio-button>
-          <el-radio-button label="广州"></el-radio-button>
-          <el-radio-button label="深圳"></el-radio-button>
+      <div class="initial">
+        <p>首字母：</p>
+        <el-radio-group size="mini" v-model="initial">
+          <el-radio-button label="热门"></el-radio-button>
+          <el-radio-button label="A"></el-radio-button>
+          <el-radio-button label="B"></el-radio-button>
+          <el-radio-button label="C"></el-radio-button>
+          <el-radio-button label="D"></el-radio-button>
+          <el-radio-button label="E"></el-radio-button>
+          <el-radio-button label="F"></el-radio-button>
+          <el-radio-button label="G"></el-radio-button>
+          <el-radio-button label="H"></el-radio-button>
+          <el-radio-button label="I"></el-radio-button>
+          <el-radio-button label="J"></el-radio-button>
+          <el-radio-button label="K"></el-radio-button>
+          <el-radio-button label="L"></el-radio-button>
+          <el-radio-button label="M"></el-radio-button>
+          <br>
+          <el-radio-button label="N"></el-radio-button>
+          <el-radio-button label="O"></el-radio-button>
+          <el-radio-button label="P"></el-radio-button>
+          <el-radio-button label="Q"></el-radio-button>
+          <el-radio-button label="R"></el-radio-button>
+          <el-radio-button label="S"></el-radio-button>
+          <el-radio-button label="T"></el-radio-button>
+          <el-radio-button label="U"></el-radio-button>
+          <el-radio-button label="V"></el-radio-button>
+          <el-radio-button label="W"></el-radio-button>
+          <el-radio-button label="X"></el-radio-button>
+          <el-radio-button label="Y"></el-radio-button>
+          <el-radio-button label="Z"></el-radio-button>
+          <el-radio-button label="#"></el-radio-button>
         </el-radio-group>
-      </div> -->
+      </div>
+      <el-divider></el-divider>
     </div>
     <div class="singer-list">
-      <!-- <singer-item v-for="(item,index) in singers" :key="index" :item="item" /> -->
-      歌手
-      <el-scrollbar></el-scrollbar>
+      
+      <!-- <el-scrollbar></el-scrollbar> -->
+      <singer-item v-for="(item,index) in singers" :key="index" :item="item"/>
     </div>
   </div>
 </template>
@@ -56,61 +84,69 @@ export default {
   },
   data() {
     return {
-      tags: [
-        { name: "全部歌手", id: "0" },
-        { name: "入驻歌手", id: "5001" },
-        { name: "华语男歌手", id: " 1001" },
-        { name: "华语女歌手", id: " 1002" },
-        { name: "华语组合/乐队", id: " 1003" },
-        { name: "欧美男歌手", id: " 2001" },
-        { name: "欧美女歌手", id: " 2002" },
-        { name: "欧美组合/乐队", id: " 2003" },
-        { name: "日本男歌手", id: " 6001" },
-        { name: "日本女歌手", id: " 6002" },
-        { name: "日本组合/乐队", id: " 6003" },
-        { name: "韩国男歌手", id: " 7001" },
-        { name: "韩国女歌手", id: " 7002" },
-        { name: "韩国组合/乐队", id: " 7003" },
-        { name: "其他男歌手", id: " 4001" },
-        { name: "其他女歌手", id: " 4002" },
-        { name: "其他组合/乐队", id: " 4003" }
+      language: '全部',
+      gender: '全部',
+      initial: '热门',
+      singers: [
+        "歌手1",
+        "歌手1",
+        "歌手1",
+        "歌手1",
+        "歌手1",
+        "歌手1",
+        "歌手1",
+        "歌手1",
+        "歌手1",
       ],
-      singers: [],
       page: 0,
       cat: 0,
       activeIndex: 0
     };
   },
-  mounted() {
-    this.getSingerlist();
-  },
-  methods: {
-    getSingerlist() {
-      singerlist(this.cat, this.page).then(res => {
-        console.log("---singerlist ", this.page, res);
-        this.singers = res.artists;
-        this.page++;
-      });
-    },
-    catClick(id, activeCat) {
-      console.log("--cat ", id);
-      this.cat = id;
-      this.activeIndex = activeCat;
-      // this.$emit('newCat')
-      this.getSingerlist();
-    }
-  },
-  watch: {
-    cat(nv, ov) {
-      if (nv != ov) {
-        this.page = 0;
-      }
-    }
-  }
+  // mounted() {
+  //   this.getSingerlist();
+  // },
+  // methods: {
+  //   getSingerlist() {
+  //     singerlist(this.cat, this.page).then(res => {
+  //       console.log("---singerlist ", this.page, res);
+  //       this.singers = res.artists;
+  //       this.page++;
+  //     });
+  //   },
+  //   catClick(id, activeCat) {
+  //     console.log("--cat ", id);
+  //     this.cat = id;
+  //     this.activeIndex = activeCat;
+  //     // this.$emit('newCat')
+  //     this.getSingerlist();
+  //   }
+  // },
+  // watch: {
+  //   cat(nv, ov) {
+  //     if (nv != ov) {
+  //       this.page = 0;
+  //     }
+  //   }
+  // }
 };
 </script>
 <style>
+.singer-page{
+  display: flex;
+  flex-direction: column;
+}
 .class-list {
-  /* size: 20px, */
+  display: flex;
+  flex-direction: column;
+}
+.singer-list{
+  /* padding: 0 20px; */
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.language, .gender, .initial{
+  display: flex;
 }
 </style>
